926cfe610782e7ffaf5d165932851149ae526a45
mdmasum4713 <mdmasum4713@gmail.com>
2025-02-26 07:45:58 +0600

    contract and news page added

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,8 @@
 import App from '@/App'
 import Layout from '@/layout/Layout'
 import AboutPage from '@/pages/about'
+import Bikes from '@/pages/Bikes'
 import Login from '@/pages/Login'
-import { News } from '@/pages/news'
 import Register from '@/pages/Register'
 import { createBrowserRouter } from 'react-router-dom'
 import BikeDetails from "../pages/BikeDetails"
@@ -17,6 +17,8 @@ import SupportHelp from '@/pages/DashBoard/SupportHelp'
 import UserProfile from '@/pages/DashBoard/UserProfile'
 import AddProduct from '@/pages/DashBoard/AddProduct'
 import UpdateProduct from '@/pages/DashBoard/UpdateProduct'
+import { News } from '@/pages/News'
+import Contract from '@/pages/Contract'
 
 const router = createBrowserRouter([
   {
@@ -36,24 +38,75 @@ const router = createBrowserRouter([
         path: '/about',
         element: <AboutPage />,
       },
-     {
-      path: 'news',
-      element: <News></News>
-     },
       {
-        path: 'contact',
-        element: <h2>Contact</h2>,
+        path: 'news',
+        element: <News></News>
+       },
+        {
+          path: 'contact',
+          element: <Contract></Contract>
+        },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
       },
     ],
   },
   {
-    path: 'login',
-    element: <Login />,
-  },
-  {
-    path: 'register',
-    element: <Register />,
-  },
+    path: '/dashBoard',
+    element: <DashBoard />,
+    children: [
+      
+      {
+        path: '/dashBoard/overView',
+        element: <OverView></OverView>,
+      },
+      {
+        path: '/dashBoard/userManagement',
+        element: <ManageUser></ManageUser>,
+      },
+      {
+        path: '/dashBoard/product-management',
+        element: <ManageProduct></ManageProduct>,
+      },
+      {
+        path: '/dashBoard/order-management',
+        element: <ManageOrders></ManageOrders>,
+      },
+      {
+        path: '/dashBoard/payment-management',
+        element: <ManagePayments></ManagePayments>,
+      },
+      {
+        path: '/dashBoard/addProduct',
+        element: <AddProduct></AddProduct>,
+      },
+      {
+        path: '/dashBoard/updateProduct/:id',
+        element: <UpdateProduct></UpdateProduct>,
+      },
+
+
+      // user 
+      {
+        path: '/dashBoard/myProfile',
+        element: <UserProfile></UserProfile>,
+      },
+      {
+        path: '/dashBoard/myOrder',
+        element: <MyOrder></MyOrder>,
+      },
+      {
+        path: '/dashBoard/support',
+        element: <SupportHelp></SupportHelp>,
+      },
+    ]
+  }
+
 ])
 
 export default router
